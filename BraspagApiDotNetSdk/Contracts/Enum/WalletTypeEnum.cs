3acc4c65fc6db76c171dd3020d2bd5d937966b5e
Adjailson Oliveira <adjailson.oliveira@gmail.com>
2020-07-07 16:27:41 -0300

    Create WalletTypeEnum.cs

---
+++ BraspagApiDotNetSdk/Contracts/Enum/WalletTypeEnum.cs
@@ -0,0 +1,14 @@
+namespace BraspagApiDotNetSdk.Contracts.Enum
+{
+    public enum WalletTypeEnum : byte
+    {
+        None = 0,
+        VisaCheckout = 1,
+        Masterpass = 2,
+        ApplePay = 3,
+        SamsungPay = 4,
+        AndroidPay = 5,
+        CieloPay = 6,
+        LinkPay = 7
+    }
+}
